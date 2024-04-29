fbc4095a60221d1984332f913e0ea3b7d9d61958
Sony Chen <hello@sony-chen.com>
2024-04-29 13:46:57 -0600

    Updated UI style

--- script.js
+++ script.js
@@ -5,29 +5,65 @@
 
 /* Your credit card */
 const creditCard = { balance: 0, limit: 2000 };
+const balanceEl = document.querySelector(".balance-amount");
 
 /* Products you can buy */
 const products = [
-    { name: "T-shirt", price: 20, numberPurchased: 0 },
-    { name: "Handbag", price: 200, numberPurchased: 0 },
-    { name: "Computer", price: 2000, numberPurchased: 0 }
+    { image: "./asset/t-shirt.svg", name: "T-shirt", price: 20, numberPurchased: 0 },
+    { image: "./asset/bag.svg", name: "Handbag", price: 200, numberPurchased: 0 },
+    { image: "./asset/computer.svg", name: "Computer", price: 2000, numberPurchased: 0 }
 ];
 
 /* Function to display the current amounts of everything in the DOM */
 function displayBalances() {
-    const balanceEl = document.querySelector(".card-balance");
-    const myProductsEl = document.querySelector(".my-products");
+    const productGroup = document.querySelector(".transaction-panel__product-group");
 
     // Display the credit card balance
-    balanceEl.innerText = "Balance: $" + creditCard.balance;
+    balanceEl.innerText = "$ " + creditCard.balance;
 
     // Display the products purchased
-    myProductsEl.innerHTML = "";
-    products.forEach((product) => {
-        const listItem = document.createElement('li');
-        listItem.innerText = product.name + ": " + product.numberPurchased;
-        myProductsEl.appendChild(listItem);
-    });
+    for (let i = 0; i < products.length; i++){
+
+        const productElm = document.createElement("div");
+        productElm.classList.add("transaction-panel__product");
+
+        const productImage = document.createElement("img");
+        productImage.src = products[i].image;
+        productElm.append(productImage);
+
+        const productPriceElm = document.createElement("div");
+        productPriceElm.classList.add("transaction-panel__product-price");
+
+        const productLabel = document.createElement("span");
+        productLabel.textContent = products[i].name;
+        productLabel.classList.add("transaction-panel__product-price--label");
+        productPriceElm.append(productLabel);
+
+        const divider = document.createElement("span");
+        divider.textContent = "|";
+        divider.classList.add("transaction-panel__product-price--divider");
+        productPriceElm.append(divider);
+
+        const price = document.createElement("span");
+        price.textContent = products[i].price;
+        price.classList.add("transaction-panel__product-price--amount");
+        productPriceElm.append(price);
+
+        const symbol = document.createElement("span");
+        symbol.textContent = "x";
+        symbol.classList.add("transaction-panel__product-price--divider");
+        productPriceElm.append(symbol);
+
+        const productAmount = document.createElement("span");
+        productAmount.textContent = products[i].numberPurchased;
+        productAmount.classList.add("transaction-panel__product-price--amount");
+        productPriceElm.append(productAmount);
+
+
+        productGroup.append(productElm);
+        productElm.append(productPriceElm);
+    }
+
 }
 
 /* Function to buy a product with your credit card */
@@ -40,22 +76,36 @@ function buyProduct(product) {
         setTimeout(processPayment, randomTime);
 
         function processPayment() {
+            const purchaseStatus = document.getElementById("status");
             // If the price is within the credit card's limit,
             // 1) charge the card, 2) purchase the product, 3) resolve the promise
             if (creditCard.balance + product.price <= creditCard.limit) {
                 creditCard.balance += product.price;
                 product.numberPurchased++;
+
+                // show the balance
+                balanceEl.innerText = "$ " + product.price;
+
+                // post status
+                const status = `🤑 Purchased ${product.name} for $${product.price}`;
+                purchaseStatus.innerHTML = status;
+                
                 return resolve({
-                    status: `Purchased ${product.name} for $${product.price}`,
+                    status,
                     timestamp: Date.now(),
                 });
-            }
+            } else {
+
+                const error = `⛔️ Declined purchase of ${product.name} for $${product.price}`;
+                purchaseStatus.innerHTML = error;
 
             // Otherwise the price exceeds the credit card's limit, so reject the promise
             reject({
-                status: `Declined purchase of ${product.name} for $${product.price}`,
+                error,
                 timestamp: Date.now()
             }); 
+            }
+
         }
     });
 }
@@ -67,8 +117,21 @@ function buyProduct(product) {
 
 displayBalances(); // You can call this pre-written function to update the DOM later
 
+const processAll = document.getElementById("process-all");
+const processInSequal = document.getElementById("process-sequal");
+const purchaseStatus = document.getElementById("status");
 
+// purchase in sequal
+const goShopping = async () => {
 
+    try{
+        const tShirtResult = await buyProduct(products[0]);
+        const handBageResult = await buyProduct(products[1]);
+        const computerResult = await buyProduct(products[2]);
 
+    } catch (error){
+        console.log(error);
+    }
+};
 
-
+processInSequal.addEventListener("click", goShopping);
